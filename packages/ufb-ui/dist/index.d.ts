08d295b5d033eff0760ed2cf2a295a8a5e4f7946
chiol <chiyoung.jeong@linecorp.com>
2023-08-23 14:27:21 +0900

    fix: only font family LineSeedEN

--- packages/ufb-ui/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './Icon';
-export * from './inputs';
-export * from './Toast';
-export * from './Badge';
-export * from './Badge';
-export * from './Tooltip';
-export * from './Dialog';
-export * from './Popover';
-//# sourceMappingURL=index.d.ts.map
--- packages/ufb-ui/dist/inputs/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './TextInput';
-export * from './Input';
-//# sourceMappingURL=index.d.ts.map
