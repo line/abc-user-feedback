ecc86bbaf93b5cd6d91bcdf6005857ecc9f7d4bb
chiol <chiyoung.jeong@linecorp.com>
2023-09-19 12:04:06 +0900

    fix: parse number type env

--- apps/web/src/env.mjs
+++ apps/web/src/env.mjs
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
 export const env = {
   NEXT_PUBLIC_API_BASE_URL:
     process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000',
@@ -7,5 +5,11 @@ export const env = {
   SESSION_PASSWORD:
     process.env.SESSION_PASSWORD ||
     'complex_password_at_least_32_characters_long',
-  NEXT_PUBLIC_MAX_DAYS: +(process.env.NEXT_PUBLIC_MAX_DAYS || 90),
+  NEXT_PUBLIC_MAX_DAYS: parseNumEnv(process.env.NEXT_PUBLIC_MAX_DAYS, 90),
 };
+
+function parseNumEnv(env, defaultvalue) {
+  const result = parseInt(env, 10);
+  if (isNaN(result)) return defaultvalue;
+  return result;
+}
