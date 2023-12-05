f7f4d64a7c5423f340ce8bf155dde9437fd29f67
chiol <chiyoung.jeong@linecorp.com>
2023-12-05 11:36:27 +0900

    fix: timeoffset and register-member i18n

--- apps/web/src/containers/create-project/InputIssueTracker.tsx
+++ apps/web/src/containers/create-project/InputIssueTracker.tsx
@@ -107,7 +107,7 @@ const InputIssueTracker: React.FC<IProps> = () => {
         userId: member.user.id,
       })),
       roles: input.roles,
-      timezoneOffset: '+90:00',
+      timezoneOffset: '+09:00',
     });
   };
 
