8764e1dfacdb1aac3aec054a32c357101c78a25b
Carson <yupyup.kim@linecorp.com>
2023-12-05 10:48:21 +0900

    impl: set statistics date by interval and fromDate (#91)

--- apps/api/src/domains/statistics/issue/issue-statistics.service.ts
+++ apps/api/src/domains/statistics/issue/issue-statistics.service.ts
@@ -19,6 +19,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CronJob } from 'cron';
 import dayjs from 'dayjs';
 import dotenv from 'dotenv';
+import { DateTime } from 'luxon';
 import { Between, Repository } from 'typeorm';
 import { Transactional } from 'typeorm-transactional';
 
@@ -69,24 +70,21 @@ export class IssueStatisticsService {
     return {
       statistics: issueStatistics.reduce(
         (acc, curr) => {
-          let endDate: dayjs.Dayjs;
-          switch (interval) {
-            case 'week':
-              endDate = dayjs(curr.date).endOf('week');
-              break;
-            case 'month':
-              endDate = dayjs(curr.date).endOf('month');
-              break;
-            default:
-              endDate = dayjs(curr.date);
-          }
+          const intervalCount = Math.floor(
+            DateTime.fromJSDate(from)
+              .until(DateTime.fromJSDate(new Date(curr.date)))
+              .length(interval),
+          );
+          const endOfInterval = DateTime.fromJSDate(from).plus({
+            [interval]: intervalCount,
+          });
 
           let statistic = acc.find(
-            (stat) => stat.date === endDate.format('YYYY-MM-DD'),
+            (stat) => stat.date === endOfInterval.toFormat('yyyy-MM-dd'),
           );
           if (!statistic) {
             statistic = {
-              date: endDate.format('YYYY-MM-DD'),
+              date: endOfInterval.toFormat('yyyy-MM-dd'),
               count: 0,
             };
             acc.push(statistic);
