82713b241bca2e40bfd9aa78b60e05c8e3245b19
yupyup-kim <yupyup.kim@linecorp.com>
2023-12-14 15:50:52 +0900

    chore: eslint

--- apps/api/src/configs/modules/typeorm-config/migrations/1700795948817-project-timezone-offset.ts
+++ apps/api/src/configs/modules/typeorm-config/migrations/1700795948817-project-timezone-offset.ts
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class ProjectTimezoneOffset1700795948817 implements MigrationInterface {
   name = 'ProjectTimezoneOffset1700795948817';
