15affb2100b47b6570b418dbf8c6af0f7b07241a
Miller <chiyoung.jeong@linecorp.com>
2024-02-20 17:32:53 +0900

    fix: optimize issue table

--- apps/web/src/containers/tables/IssueTable/IssueTabelSelectBox/IssueTabelSelectBox.tsx
+++ apps/web/src/containers/tables/IssueTable/IssueTabelSelectBox/IssueTabelSelectBox.tsx
@@ -19,12 +19,10 @@ import { IssueCircle } from '@/components';
 import { ISSUES } from '@/constants/issues';
 
 interface IProps extends React.PropsWithChildren {
-  onChangeOption: (issue: IssueCountOption) => void;
+  onChangeOption: (status: string) => void;
   currentIssueKey: string | undefined;
   issueCountData: { key: string; count: number }[] | undefined;
 }
-
-type IssueCountOption = { count: number; key: string };
 
 const IssueTabelSelectBox: React.FC<IProps> = (props) => {
   const { currentIssueKey, onChangeOption, issueCountData } = props;
@@ -33,7 +31,33 @@ const IssueTabelSelectBox: React.FC<IProps> = (props) => {
 
   return (
     <div className="flex flex-wrap gap-2">
-      {issueCountData?.map((issue) => (
+      {[{ key: 'total' }, ...ISSUES(t)].map((issue) => (
+        <div
+          key={issue.key}
+          className={[
+            'flex min-w-[136px] cursor-pointer items-center justify-between rounded border px-3 py-2.5',
+            currentIssueKey === issue.key ||
+            (currentIssueKey === undefined && issue.key === 'total')
+              ? 'border-fill-primary'
+              : 'opacity-50',
+          ].join(' ')}
+          onClick={() => onChangeOption(issue.key)}
+        >
+          <div className="flex flex-1 items-center gap-2">
+            <IssueCircle issueKey={issue.key} />
+            <span className="whitespace-nowrap">
+              {ISSUES(t).find((v) => v.key === issue.key)?.name ??
+                t('text.all')}
+            </span>
+          </div>
+          <p className="text-secondary ml-1.5">
+            {t('text.number-count', {
+              count: issueCountData?.find((v) => v.key)?.count ?? 0,
+            })}
+          </p>
+        </div>
+      ))}
+      {/* {issueCountData?.map((issue) => (
         <div
           key={issue.key}
           className={[
@@ -56,7 +80,7 @@ const IssueTabelSelectBox: React.FC<IProps> = (props) => {
             {t('text.number-count', { count: issue.count })}
           </p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
