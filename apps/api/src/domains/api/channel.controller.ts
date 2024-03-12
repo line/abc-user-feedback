cdf5aa5d0ae53fc187405bfdfcd13975f8e16f65
yupyup-kim <yupyup.kim@linecorp.com>
2024-03-13 00:33:30 +0900

    docs: api

--- apps/api/src/domains/api/channel.controller.ts
+++ apps/api/src/domains/api/channel.controller.ts
@@ -22,7 +22,13 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { ApiKeyAuthGuard } from '@/domains/admin/auth/guards';
 import { ChannelService } from '../admin/channel/channel/channel.service';
@@ -33,6 +39,11 @@ import { ChannelService } from '../admin/channel/channel/channel.service';
 export class ChannelController {
   constructor(private readonly channelService: ChannelService) {}
 
+  @ApiOperation({
+    summary: 'Get Image Upload URL',
+    description:
+      'Generates a presigned URL for image upload to a specific channel within a project.',
+  })
   @ApiParam({
     name: 'projectId',
     type: Number,
@@ -44,6 +55,13 @@ export class ChannelController {
     type: Number,
     description: 'Channel id',
     example: 1,
+  })
+  @ApiQuery({
+    name: 'extension',
+    type: String,
+    required: true,
+    description: 'File extension for the image to be uploaded',
+    example: 'png',
   })
   @ApiOkResponse({
     type: String,
