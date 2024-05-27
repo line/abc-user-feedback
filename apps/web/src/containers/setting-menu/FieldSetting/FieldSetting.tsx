05239848c09712c74d777508a1ef25ef5b5b0086
Chiyoung Jeong <chiyoung.jeong@linecorp.com>
2024-05-27 13:28:56 +0900

    fix: change setting query state for field setting confirm popup (#284)
    change setting query state

--- apps/web/src/containers/setting-menu/FieldSetting/FieldSetting.tsx
+++ apps/web/src/containers/setting-menu/FieldSetting/FieldSetting.tsx
@@ -249,6 +249,8 @@ const FieldSetting: React.FC<IProps> = ({ projectId, channelId }) => {
     const handleBeforeChangeRoute = (url: string) => {
       if (router.pathname !== url && !confirm(confirmMsg)) {
         router.events.emit('routeChangeError');
+        console.log('channelId: ', channelId);
+
         throw `사이트 변경 취소`;
       }
     };
