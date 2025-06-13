81ab42bba2ea50209fe18a221a4b97b1d75e2849
chiol <jcy6677@gmail.com>
2025-06-13 16:15:27 +0900

    feat: ui

---
+++ apps/web/src/features/update-ai-setting/ui/ai-field-template-form.ui.tsx
@@ -0,0 +1,351 @@
+/**
+ * Copyright 2025 LY Corporation
+ *
+ * LY Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { create } from 'zustand';
+
+import {
+  Button,
+  Icon,
+  InputField,
+  InputLabel,
+  Switch,
+  Textarea,
+  toast,
+} from '@ufb/react';
+
+import {
+  Card,
+  CardBody,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  SelectInput,
+  TextInput,
+  useOAIMutation,
+  useOAIQuery,
+} from '@/shared';
+import type { AITemplate } from '@/entities/ai';
+
+import type { AISettingStore } from '../ai-setting-form.type';
+
+type InputItem = {
+  title: string;
+  description: string;
+  value: string;
+};
+
+const useAITemplateFormStore = create<AISettingStore>((set) => ({
+  formId: 'ai-template-form',
+  isPending: false,
+  setIsPending: (isPending) => set({ isPending }),
+  isDirty: false,
+  setIsDirty: (isDirty) => set({ isDirty }),
+}));
+
+const AIPlaygroundContext = React.createContext<{
+  inputItems: InputItem[];
+  createInputItem: (item: InputItem) => void;
+  updateInputItem: (index: number, item: InputItem) => void;
+  deleteInputItem: (index: number) => void;
+}>({
+  inputItems: [],
+  createInputItem: () => 0,
+  updateInputItem: () => 0,
+  deleteInputItem: () => 0,
+});
+
+export const AIFieldTemplateForm = ({ projectId }: { projectId: number }) => {
+  const [inputItems, setInputItems] = useState<InputItem[]>([]);
+  const { register, formState, setValue, watch, handleSubmit } =
+    useForm<AITemplate>();
+  const { formId, setIsPending, setIsDirty } = useAITemplateFormStore();
+
+  const { data: modelData } = useOAIQuery({
+    path: '/api/admin/projects/{projectId}/ai/integrations/models',
+    variables: { projectId },
+  });
+
+  const { mutate, isPending } = useOAIMutation({
+    method: 'post',
+    path: '/api/admin/projects/{projectId}/ai/templates/new',
+    pathParams: { projectId },
+    queryOptions: {
+      onSuccess: () => {
+        toast.success('AI 설정이 저장되었습니다.');
+      },
+      onError(error) {
+        toast.error(error.message);
+      },
+    },
+  });
+
+  useEffect(() => {
+    setIsPending(isPending);
+  }, [isPending]);
+
+  useEffect(() => {
+    setIsDirty(formState.isDirty);
+  }, [formState.isDirty]);
+
+  return (
+    <div className="flex min-h-0 flex-1 gap-4">
+      <Card className="flex-[1] border" size="md">
+        <CardHeader>
+          <CardTitle>Configuration</CardTitle>
+          <CardDescription>
+            템플릿 정보와 프롬프트를 설정해주세요
+          </CardDescription>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4">
+          <form
+            id={formId}
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit((data) => {
+              mutate(data);
+            })}
+          >
+            <TextInput label="Title" {...register('title')} />
+            <InputField>
+              <InputLabel>Prompt</InputLabel>
+              <Textarea {...register('prompt')} />
+            </InputField>
+            <Card size="sm">
+              <CardHeader
+                action={
+                  <Switch
+                    checked={watch('autoProcessing')}
+                    onCheckedChange={(checked) =>
+                      setValue('autoProcessing', checked, { shouldDirty: true })
+                    }
+                  />
+                }
+              >
+                <CardTitle>Auto Processing</CardTitle>
+                <CardDescription>
+                  자동으로 AI Prompt를 적용합니다.
+                </CardDescription>
+              </CardHeader>
+            </Card>
+            <div>
+              <h4>Advanced Configuration</h4>
+              <p>description</p>
+              <SelectInput
+                options={
+                  modelData?.models.map((model) => ({
+                    value: model,
+                    label: model,
+                  })) ?? []
+                }
+                label="Model"
+                placeholder="Select a model"
+                onChange={(value) =>
+                  setValue('model', value ?? '', { shouldDirty: true })
+                }
+                value={watch('model')}
+              />
+              <SelectInput
+                options={
+                  modelData?.models.map((model) => ({
+                    value: model,
+                    label: model,
+                  })) ?? []
+                }
+                label="Model"
+                placeholder="Select a model"
+                onChange={(value) =>
+                  setValue('model', value ?? '', { shouldDirty: true })
+                }
+                value={watch('model')}
+              />
+            </div>
+          </form>
+        </CardBody>
+      </Card>
+      <AIPlaygroundContext.Provider
+        value={{
+          inputItems,
+          createInputItem: (item) => setInputItems((prev) => [...prev, item]),
+          updateInputItem: (index, item) =>
+            setInputItems((prev) => {
+              const newData = [...prev];
+              newData[index] = item;
+              return newData;
+            }),
+          deleteInputItem: (index) =>
+            setInputItems((prev) => {
+              const newData = [...prev];
+              newData.splice(index, 1);
+              return newData;
+            }),
+        }}
+      >
+        <Card
+          className="last-of-type: flex h-full flex-[2] flex-col border"
+          size="md"
+        >
+          <CardHeader>
+            <CardTitle>Playground</CardTitle>
+            <CardDescription>
+              테스트 데이터와 템플릿 가지고 AI 결과를 미리 확인해 보세요.
+            </CardDescription>
+          </CardHeader>
+          <CardBody className="flex min-h-0 flex-1 flex-col gap-4">
+            <Card size="sm" className="flex min-h-0 flex-[2] flex-col">
+              <CardHeader
+                action={
+                  <Button
+                    variant="outline"
+                    onClick={() =>
+                      setInputItems((prev) => [
+                        ...prev,
+                        { title: '', description: '', value: '' },
+                      ])
+                    }
+                  >
+                    <Icon name="RiAddLine" />
+                    테스트 케이스 추가
+                  </Button>
+                }
+              >
+                <CardTitle>Input Data</CardTitle>
+              </CardHeader>
+              <CardBody className="flex min-h-0 flex-1 flex-col gap-4 overflow-auto">
+                {inputItems.map((_, key) => (
+                  <AIPlaygroundInputDataItem key={key} index={key} />
+                ))}
+              </CardBody>
+            </Card>
+            <Card size="sm" className="flex-[1]">
+              <CardHeader
+                action={
+                  <Button variant="outline">
+                    <Icon name="RiSparklingFill" />
+                    AI 테스트
+                  </Button>
+                }
+              >
+                <CardTitle>Output</CardTitle>
+              </CardHeader>
+            </Card>
+          </CardBody>
+        </Card>
+      </AIPlaygroundContext.Provider>
+    </div>
+  );
+};
+
+export const AITemplateFormButton = () => {
+  const { formId, isPending, isDirty } = useAITemplateFormStore();
+  return (
+    <Button form={formId} type="submit" loading={isPending} disabled={!isDirty}>
+      저장
+    </Button>
+  );
+};
+
+const AIPlaygroundInputDataItem = ({ index }: { index: number }) => {
+  const { inputItems, updateInputItem, deleteInputItem } =
+    React.useContext(AIPlaygroundContext);
+  const [isEditing, setIsEditing] = useState(true);
+  const data = inputItems[index];
+
+  if (!data) return;
+
+  if (isEditing) {
+    return (
+      <AIPlaygroundInputDataItemForm
+        initialValues={data}
+        onClose={() => {
+          if (data.title === '') {
+            deleteInputItem(index);
+          }
+          setIsEditing(false);
+        }}
+        onSubmit={(data) => {
+          updateInputItem(index, data);
+          setIsEditing(false);
+        }}
+      />
+    );
+  }
+
+  return (
+    <Card size="sm">
+      <CardHeader
+        action={
+          <div className="flex gap-2">
+            <Button variant="outline" onClick={() => setIsEditing(true)}>
+              <Icon name="RiEditBoxLine" />
+            </Button>
+            <Button onClick={() => deleteInputItem(index)} variant="outline">
+              <Icon name="RiDeleteBinLine" />
+            </Button>
+          </div>
+        }
+      >
+        <CardTitle>{data.title}</CardTitle>
+        <CardDescription>{data.description}</CardDescription>
+      </CardHeader>
+      <CardBody>
+        <div className="bg-neutral-tertiary rounded-8 max-h-16 overflow-auto p-3">
+          <p className="text-small-normal text-neutral-secondary">
+            {data.value}
+          </p>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
+
+const AIPlaygroundInputDataItemForm = ({
+  onSubmit,
+  onClose,
+  initialValues,
+}: {
+  onSubmit: (input: InputItem) => void;
+  onClose: () => void;
+  initialValues?: InputItem;
+}) => {
+  const { register, handleSubmit, reset } = useForm<InputItem>({
+    defaultValues: initialValues ?? { title: '', description: '', value: '' },
+  });
+
+  return (
+    <Card size="sm">
+      <CardBody>
+        <form className="flex flex-col gap-1" onSubmit={handleSubmit(onSubmit)}>
+          <TextInput label="Field Title" {...register('title')} />
+          <TextInput label="Field Description" {...register('description')} />
+          <TextInput label="Field Value" {...register('value')} />
+          <div className="flex justify-end gap-2">
+            <Button
+              onClick={() => {
+                reset();
+                onClose();
+              }}
+              variant="outline"
+            >
+              Cancel
+            </Button>
+            <Button type="submit">Confirm</Button>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
+  );
+};
---
+++ apps/web/src/features/update-ai-setting/ui/ai-field-template-setting.ui.tsx
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2025 LY Corporation
+ *
+ * LY Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Icon } from '@ufb/react';
+
+import {
+  Card,
+  CardBody,
+  HelpCardDocs,
+  SettingAlert,
+  useOAIQuery,
+} from '@/shared';
+
+export const AIFieldTemplateSetting = ({
+  onClick,
+  projectId,
+}: {
+  onClick: () => void;
+  projectId: number;
+}) => {
+  const { data } = useOAIQuery({
+    path: '/api/admin/projects/{projectId}/ai/templates',
+    variables: { projectId },
+  });
+
+  return (
+    <>
+      <SettingAlert
+        description={<HelpCardDocs i18nKey="help-card.api-key" />}
+      />
+      <div className="grid grid-cols-4 gap-4">
+        <TemplateCard type="create" title="New Template" onClick={onClick} />
+        {data?.map(({ id, title, prompt }) => (
+          <TemplateCard
+            key={id}
+            type="update"
+            title={title}
+            description={prompt}
+            onClick={onClick}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const TemplateCard = (props: {
+  title: string;
+  description?: string;
+  type: 'create' | 'update';
+  onClick?: () => void;
+}) => {
+  const { title, description, type, onClick } = props;
+  return (
+    <Card
+      onClick={onClick}
+      className="min-h-60 cursor-pointer hover:opacity-50"
+    >
+      <CardBody className="flex h-full flex-col justify-between">
+        <div className="flex flex-col gap-1">
+          {type === 'create' && <Icon name="RiAddCircleFill" />}
+          {type === 'update' && <StarIcon />}
+          <h4 className="text-title-h4">{title}</h4>
+        </div>
+        {description && (
+          <div>
+            <p className="text-small-normal">Prompt Preview</p>
+            <div className="bg-neutral-tertiary rounded-12 p-3">
+              <p className="text-small-normal line-clamp-2">{description}</p>
+            </div>
+          </div>
+        )}
+      </CardBody>
+    </Card>
+  );
+};
+
+const StarIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.1973 14.5996C18.4547 16.2456 19.7544 17.5453 21.4004 17.8027V18.1963C19.7543 18.4537 18.4547 19.7543 18.1973 21.4004H17.8027C17.5453 19.7543 16.2457 18.4537 14.5996 18.1963V17.8027C16.2456 17.5453 17.5453 16.2456 17.8027 14.5996H18.1973ZM8.42773 5.59961C8.71293 8.76612 11.2339 11.286 14.4004 11.5713V12.4277C11.2339 12.7129 8.71294 15.2339 8.42773 18.4004H7.57227C7.28706 15.2339 4.76614 12.7129 1.59961 12.4277V11.5713C4.7661 11.2861 7.28707 8.76612 7.57227 5.59961H8.42773ZM17.0225 2.59961C17.2621 3.79603 18.204 4.73793 19.4004 4.97754V5.02148C18.2039 5.26103 17.2621 6.2039 17.0225 7.40039H16.9775C16.7379 6.2039 15.7961 5.26103 14.5996 5.02148V4.97754C15.796 4.73793 16.7379 3.79603 16.9775 2.59961H17.0225Z"
+        fill="url(#paint0_linear_12816_270031)"
+        stroke="url(#paint1_linear_12816_270031)"
+        stroke-width="1.2"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_12816_270031"
+          x1="11.5"
+          y1="2"
+          x2="11.5"
+          y2="22"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#2DD4BF" />
+          <stop offset="1" stop-color="#0EA5E9" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_12816_270031"
+          x1="11.5"
+          y1="2"
+          x2="11.5"
+          y2="22"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#2DD4BF" />
+          <stop offset="1" stop-color="#0EA5E9" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
