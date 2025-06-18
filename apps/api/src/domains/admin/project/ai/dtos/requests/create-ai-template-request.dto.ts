75309b02247d3870db58641be633d70c40509c60
jeehoon.choi <jeehoon.choi@linecorp.com>
2025-06-18 13:29:42 +0900

    fix lint

--- apps/api/src/domains/admin/project/ai/dtos/requests/create-ai-template-request.dto.ts
+++ apps/api/src/domains/admin/project/ai/dtos/requests/create-ai-template-request.dto.ts
@@ -14,7 +14,7 @@
  * under the License.
  */
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsString, MaxLength } from 'class-validator';
+import { IsNumber, IsString, MaxLength } from 'class-validator';
 
 export class CreateAITemplateRequestDto {
   @ApiProperty({ nullable: false, type: String })
