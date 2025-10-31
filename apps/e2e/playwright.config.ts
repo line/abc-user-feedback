bec8d732817344a2c88e84fd5c6e2a2ee2f4c925
jeehoon.choi <jeehoon.choi@linecorp.com>
2025-10-31 13:55:02 +0900

    fix test

--- apps/e2e/playwright.config.ts
+++ apps/e2e/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 15 * 1000,
+    timeout: 60 * 1000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
