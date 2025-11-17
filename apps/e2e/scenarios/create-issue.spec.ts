ac80a84e02e1a31bc0e2cb8dc1b9dc792ed59511
jeehoon.choi <jeehoon.choi@linecorp.com>
2025-11-17 13:41:56 +0900

    fix test

--- apps/e2e/scenarios/create-issue.spec.ts
+++ apps/e2e/scenarios/create-issue.spec.ts
@@ -64,7 +64,7 @@ export default () => {
         .locator('button[data-slot="popover-trigger"]:has-text("+")')
         .first()
         .click();
-      await page.waitForTimeout(1000);
+      await page.waitForTimeout(2000);
 
       await page.getByText('test_issue').first().click();
       await page.waitForTimeout(1000);
